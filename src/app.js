734a3ec54efa2bee3a490eb47778d3f07324a072
tenubar <solartech4756@gmail.com>
2025-03-14 01:23:20 -0400

    34322

--- src/app.js
+++ src/app.js
@@ -23,12 +23,8 @@ const io = new Server(server);
 const router = Router();
 const PORT = 3000;
 
-app.use(cors({
-    origin: "https://agent-workflow-page.onrender.com", // Your frontend domain
-    methods: ["GET", "POST"],
-    credentials: true // Allow cookies or authentication tokens
-}));
-app.options("*", cors());
+
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
